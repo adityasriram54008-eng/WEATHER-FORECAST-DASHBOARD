88047f121b21b9634eb3d9abf4c82ed44ea6cb33
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-21 21:46:23 +0530

    fixes

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "//Your API KEY//"
 
-def get_data(place, forecast_days = None):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -14,4 +14,4 @@ def get_data(place, forecast_days = None):
 
 
 if __name__ == "__main__":
-    print(get_data(place = "Tokyo"))
+    print(get_data(place = "Tokyo", forecast_days=2))
