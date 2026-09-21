3ce281de27f4cec822360554c57dfe1164ddb266
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-21 19:28:36 +0530

    implemented get_data(), to get data from openweathermap api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the upcoming days")
 place = st.text_input("Place: ")
@@ -9,6 +10,8 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help = "Select the n
 option = st.selectbox("Select data to view",("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+data = get_data(place, days,option)
 
 dates = ["1","2"]
 temperatures = ["26","35"]
